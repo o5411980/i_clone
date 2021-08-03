c301c330bbd16a5f474437cc0bbaf449f21ef456
yoichi ogo <o541.1980@gmail.com>
2021-08-03 10:34:50 +0900

    画像、テキスト投稿機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pictures
   root to: 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
