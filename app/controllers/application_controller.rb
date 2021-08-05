57972a9f320e68390c08b17506b5403c03f2637a
yoichi ogo <o541.1980@gmail.com>
2021-08-05 16:21:17 +0900

    アクセス制限を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,8 @@ class ApplicationController < ActionController::Base
   redirect_to new_session_path unless current_user
   end
   def my_page_access
-#    if current_user.id.to_s != params[:id]
     if current_user.id != Picture.find_by(id: params[:id]).user_id && current_user.id.to_s != params[:id] && current_user.id != params[:id]
     redirect_to user_path(current_user.id)
-    binding.pry
     end
   end
 end
